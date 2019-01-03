53450f564cf70ae88cf702b8de1b21a632234a43
keripukki <arttu.kosonen@wsp.com>
2019-01-03 10:21:48 +0200

    init unit test file for tsa

---
+++ test_tsa.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Unit tests for tsa module.
+"""
+
+import unittest
+from tsa import *
+
+class TestTsa(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	
+
+if __name__ == '__main__':
+	unittest.main()
