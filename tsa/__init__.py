b0008166b88221ab8eb16c025beb9bc3be6b1007
keripukki <arttu.kosonen@wsp.com>
2019-09-11 13:35:10 +0300

    Add lower level imports

--- tsa/__init__.py
+++ tsa/__init__.py
@@ -2,5 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from .utils import tsadb_connect
+from .block import Block
+from .condition import Condition
 from .cond_collection import CondCollection
 from .analysis_collection import AnalysisCollection
