e214d748b5df2139760ba214217513756bbeb282
keripukki <arttu.kosonen@wsp.com>
2019-10-17 20:15:15 +0300

    Introduce generic error class
    For non-fatal errors; fatal ones should be raised and interrupt the 
    analysis

---
+++ tsa/error.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+from datetime import datetime
+
+log = logging.getLogger(__name__)
+
+class TsaError:
+    """
+    Store non-fatal errors that can be saved to a log
+    or printed after an analysis run, without interrupting the analysis.
+    """
+    def __init__(self, msg, context, log_add=''):
+        self.msg = msg
+        self.context = context
+        self.timestamp = datetime.now()
+        self.n_more = 0
+        if log_add == '':
+            pass
+        elif log_add == 'warning':
+            log.warning(msg)
+        elif log_add == 'exception':
+            log.exception(msg)
+        else:
+            log.error(msg)
+
+    def __str__(self):
+        s = f'{self.timestamp}; {self.context}: {self.msg}'
+        if self.n_more > 0:
+            s += f' ({self.n_more} more similar errors)'
+        return s
+
+    def __repr__(self):
+        return '<TsaError> ' + str(self)
+
+    def __eq__(self, other):
+        return self.msg, self.context == other.msg, other.context
+
+    def __gt__(self, other):
+        return self.timestamp > other.timestamp
+
+class TsaErrCollection:
+    """
+    Container for errors of a tsa object.
+    Provides methods for sorting and printing errors.
+    """
+    def __init__(self, context):
+        self.context = context
+        self.errors = list()
+
+    def add(self, msg, log_add=''):
+        """
+        Add error while preventing duplicate errors;
+        for duplicates, only increase the first one's ``.n_more`` for printing.
+        """
+        e = TsaError(msg, self.context, log_add)
+        if e in self.errors:
+            self.errors[self.errors.index(e)].n_more += 1
+        else:
+            self.errors.append(e)
+
+    def short_str(self):
+        """
+        Collect error messages to one line in time order.
+        """
+        errs = [e.msg for e in sorted(self.errors)]
+        return '; '.join(errs)
+
+    def __str__(self):
+        return '\n'.join([str(e) for e in sorted(self.errors)])
+
+    def __repr__(self):
+        return f'<TsaErrCollection> with {len(self.errors)} errors'
