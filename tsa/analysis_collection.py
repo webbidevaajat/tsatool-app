209f0ea4e5d96e44d724986bb4db8589adfde8d7
keripukki <arttu.kosonen@wsp.com>
2019-10-28 12:28:24 +0200

    Add str method

--- tsa/analysis_collection.py
+++ tsa/analysis_collection.py
@@ -228,3 +228,15 @@ class AnalysisCollection:
             wb.save(wb_outpath)
             log.info(f'Excel results saved to {wb_outpath}')
         log.info(f'END OF ANALYSES for analysis collection {self.name}')
+
+
+    def __str__(self):
+        s = f'AnalysisCollection <{self.name}> based on <{self.input_xlsx}>: '
+        s += f'created at {str(self.created_at)}, '
+        s += f'{len(self.collections)} collections, '
+        if self.pg_conn is None:
+            s += 'not connected to DB, '
+        else:
+            s += 'connected to DB, '
+        s += f'{len(self.errors)} errors'
+        return s
